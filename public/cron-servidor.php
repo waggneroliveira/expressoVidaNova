349f6363e4de2b199126564837f21c9b504b19bb
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2026-01-26 19:08:05 -0300

    Update cron-servidor.php

--- public/cron-servidor.php
+++ public/cron-servidor.php
@@ -1,28 +1,31 @@
 <?php
-    if (!isset($_GET['token']) || $_GET['token'] !== 'fbaffa5c0ac7f47a89abdf8fa3eb4aa7') {
-        http_response_code(403);
-        exit('Acesso negado');
-    }
 
-    echo "<h3>CRON RSS - Laravel 11</h3>";
+$token = $_GET['token']
+    ?? ($_SERVER['HTTP_X_CRON_AUTH'] ?? null);
 
-    $baseDir = realpath(__DIR__ . '/../');
-    echo "<strong>Base dir:</strong> {$baseDir}<br><br>";
+if ($token !== 'fbaffa5c0ac7f47a89abdf8fa3eb4aa7') {
+    http_response_code(403);
+    exit('Acesso negado');
+}
 
-    $commands = [
-        'rss:g1bahia',
-        'rss:govba',
-        'rss:bahianoticias',
-    ];
+echo "<h3>CRON RSS - Laravel 11</h3>";
 
-    foreach ($commands as $command) {
-        echo "<strong>Rodando:</strong> php artisan {$command}<br>";
+$baseDir = realpath(__DIR__ . '/../');
+$phpBin  = '/usr/bin/php';
 
-        $output = shell_exec("cd {$baseDir} && php artisan {$command} 2>&1");
+$commands = [
+    'rss:g1bahia',
+    'rss:govba',
+    'rss:bahianoticias',
+];
 
-        echo "<pre>";
-        echo htmlspecialchars($output ?: 'Executado sem retorno');
-        echo "</pre><hr>";
-    }
+foreach ($commands as $command) {
+    echo "<strong>Rodando:</strong> {$command}<br>";
 
-    echo "<strong>Finalizado em:</strong> " . date('d/m/Y H:i:s');
+    $cmd = "cd {$baseDir} && {$phpBin} artisan {$command} --no-interaction 2>&1";
+    $output = shell_exec($cmd);
+
+    echo "<pre>" . htmlspecialchars($output ?: 'Executado sem retorno') . "</pre><hr>";
+}
+
+echo "<strong>Finalizado em:</strong> " . date('d/m/Y H:i:s');
