bf44ab42f8ea5c7cd4c58b4f3105ba4476112b3a
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2026-01-26 21:00:10 -0300

    Update cron-servidor.php

--- public/cron-servidor.php
+++ public/cron-servidor.php
@@ -1,23 +1,18 @@
+#!/usr/bin/env php
 <?php
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
-
-// Autoload
 require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap do Laravel 11
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Inicializa a aplicação (Kernel)
-$kernel = $app->make(Illuminate\Foundation\Application::class);
-$kernel->bootstrap();
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
-// Executa os comandos Artisan
-Artisan::call('rss:g1bahia');
-Artisan::call('rss:govba');
-Artisan::call('rss:bahianoticias');
+$status = $kernel->handle(
+    $input = new Symfony\Component\Console\Input\ArrayInput([
+        'command' => 'schedule:run',
+    ]),
+    new Symfony\Component\Console\Output\ConsoleOutput()
+);
 
-Log::info('Cron RSS executado via cron.php');
-
-echo 'OK';
+$kernel->terminate($input, $status);
+exit($status);
