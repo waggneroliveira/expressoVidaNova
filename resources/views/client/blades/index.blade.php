21ea98b698106ff29c3608d68546451df60b10d8
Front-end-4 <31347903+waggneroliveira@users.noreply.github.com>
2026-02-02 17:20:36 -0300

    Update index.blade.php

--- resources/views/client/blades/index.blade.php
+++ resources/views/client/blades/index.blade.php
@@ -43,7 +43,7 @@
     </script>
 @endif
 
-<script>
+<script defer>
     // Função para traduzir todos os elementos do widget
     function translateWidgetElements() {
     // Alterar "Standings" para "Classificação"
@@ -90,7 +90,7 @@
     }, 10000);
 </script>
 
-<script type="module" src="https://widgets.tradingview-widget.com/w/br/tv-ticker-tape.js"></script>
+<script defer type="module" src="https://widgets.tradingview-widget.com/w/br/tv-ticker-tape.js"></script>
 <tv-ticker-tape symbols='FOREXCOM:SPXUSD,FOREXCOM:NSXUSD,FOREXCOM:DJI,FX:EURUSD,BITSTAMP:BTCUSD,BITSTAMP:ETHUSD,CMCMARKETS:GOLD' show-hover theme="light"></tv-ticker-tape>
 
 <!-- TradingView Widget END -->
